bd539d5b8971fc21a7a9456d37800fc181d7b633
karen <karenlisethgutierrezlopez@gmail.com>
2026-08-04 14:27:30 -0500

    Ejercicio2: (promedio y estado del curso), esta finalizado y funcionando

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -2,14 +2,23 @@ import javax.swing.*;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        double acumulador;
+        double acumulador= 0;
         double promedio;
         int numestudiantes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de estudiantes"));
         double [] notas = new double[numestudiantes];
 
         for (int i = 0; i < numestudiantes; i++) {
-            JOptionPane.showInputDialog("Ingrese las notas");
+            notas[i] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las nota"));
 
+            acumulador += notas[i];
+        }
+
+        promedio = acumulador / numestudiantes;
+
+        if (promedio >= 3.0) {
+            JOptionPane.showMessageDialog(null, "El promedio es " +promedio + "\n Estado: Aprobado");
+        }else {
+            JOptionPane.showMessageDialog(null, "El promedio es " +promedio + "\n Estado: Reprobado");
         }
     }
 }
