7d011237a75bde23e38d303fb3ef554bc9c682a3
karen <karenlisethgutierrezlopez@gmail.com>
2026-08-04 14:15:52 -0500

    Ejercicio2: (promedio y estado del curso), va en la mitad del codigo

---
+++ src/Ejercicio2.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        double acumulador;
+        double promedio;
+        int numestudiantes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de estudiantes"));
+        double [] notas = new double[numestudiantes];
+
+        for (int i = 0; i < numestudiantes; i++) {
+            JOptionPane.showInputDialog("Ingrese las notas");
+
+        }
+    }
+}
