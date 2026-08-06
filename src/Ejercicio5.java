9edb6eb29513909de1ab73573729cf6231b6d3b9
karen <karenlisethgutierrezlopez@gmail.com>
2026-08-06 13:35:06 -0500

    Ejercicio5 (Busqueda de un numero en el array) very complicado no lo entendi del todo asi que pedi ayuda y acabe

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -9,7 +9,21 @@ public class Ejercicio5 {
         for(int i = 0; i < num; i++){
             numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero"));
 
+        }
+        int buscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que desea encontrar"));
+        boolean encontrado = false;
+        String resultado = "";
 
+        for(int i = 0; i < num; i++){
+
+            if(numeros[i] == buscar){
+                encontrado = true;
+                resultado += "El número " + buscar + " se encontró en la posición  " + i + "\n";
+            }
+        }
+        if(encontrado == false){
+            JOptionPane.showMessageDialog(null, "El numero no fue encontrado");
         }
     }
 }
+
